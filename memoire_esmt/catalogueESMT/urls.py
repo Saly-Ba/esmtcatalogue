ced108a26787341d28c31c6c17f2553d863cdc2b
Saly-Ba <sellyba7@gmail.com>
2020-12-23 14:05:39 +0000

    2nd update today

--- memoire_esmt/catalogueESMT/urls.py
+++ memoire_esmt/catalogueESMT/urls.py
@@ -6,5 +6,10 @@ app_name = "catalogueESMT"
 urlpatterns = [
     path('',views.acceuil,name="acceuil"),
     path('inscription/',views.inscription_participant,name="inscription_participant"),
+    path('login/',views.login,name="login"),
+    path('participant/',views.espace_participant,name="participant"),
+    path('animateur/',views.espace_animateur,name="animateur"),
+    path('gestion/',views.espace_gestionnaire,name="gestionnaire"),
     path('gestion/ajout/animateur',views.ajout_animateur,name="ajout_animateur"),
+
 ]
--- memoire_esmt/catalogueESMT/views.py
+++ memoire_esmt/catalogueESMT/views.py
@@ -8,6 +8,28 @@ from .forms import ParticipantForm,AnimateurForm,GestionnaireForm,FormationForm,
 def acceuil(request):
     return render(request,"catalogueESMT/acceuil.html")
 
+def login(request):
+    if request.method == "POST":
+        user = User.objects.get(username=request.POST['username'])
+        if(user.username == request.POST['username'] and user.password == request.POST['password']):
+            request.session['pk'] = user.pk
+            if user.user_type == 1:
+                return redirect('catalogueESMT:gestionnaire')
+            elif user.user_type == 2:
+                return redirect('catalogueESMT:animateur')
+            else :
+                return redirect('catalogueESMT:participant')
+        
+    return render(request,'registration/login.html')
+
+def logout(request):
+    try:
+        del request.session['pk']
+    except KeyError:
+        pass
+    return redirect('catalogueESMT:acceuil')
+
+#Participant Actions :
 def inscription_participant(request):
     if request.method == "POST":
         form = ParticipantForm(request.POST)
@@ -18,36 +40,29 @@ def inscription_participant(request):
         form = ParticipantForm()
     return render(request,'catalogueESMT/participant/inscriptionParticipant.html', {'form' : form})
 
-def login(request):
-    if request.method == "POST":
-        user = User.objects.get(username=request.POST['username'])
-        for user in users:
-            if(user.login == request.POST['login'] and user.pwd == request.POST['pwd']):
-                request.session['pk'] = user.pk
-                return redirect('gestionnaire_depenses:espace_utilisateur')
+def espace_participant(request):
+    participant = User.objects.get(pk=request.session['pk'])
+    return render(request,'catalogueESMT/participant/espace_participant.html',{'participant':participant})
+
+#Animateurs Actions :
+def espace_animateur(request):
+    animateur = User.objects.get(pk=request.session['pk'])
+    return render(request,'catalogueESMT/animateur/espace_animateur.html',{'animateur':animateur})
+
+#Gestionnaire Actions :
+def espace_gestionnaire(request):
+    gestionnaire = User.objects.get(pk=request.session['pk'])
+    return render(request,'catalogueESMT/gestionnaire/espace_gestionnaire.html',{'gestionnaire':gestionnaire})
         
-        #print("On a pu nous connecter, veuillez verifier vous identifiants...")
-    return render(request,'registration/login.html')
-
-def logout(request):
-    try:
-        del request.session['pk']
-    except KeyError:
-        pass
-    return redirect('gestionnaire_depenses:home')
-
-
 def ajout_animateur(request):
-    #connected_user = User.objects.get(pk=request.session['pk'])
-   # if connected_user.user_type == 1:
-    print("Heyyy !!!")
-    if request.method == "POST":
-        form = AnimateurForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('catalogueESMT:acceuil')
-    else:
-        form = AnimateurForm()
+    connected_user = User.objects.get(pk=request.session['pk'])
+    if connected_user.user_type == 1:
+        print("Heyyy !!!")
+        if request.method == "POST":
+            form = AnimateurForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('catalogueESMT:acceuil')
+        else:
+            form = AnimateurForm()
     return render(request,'catalogueESMT/animateur/ajout_animateur.html',{'form':form})
-        
-
