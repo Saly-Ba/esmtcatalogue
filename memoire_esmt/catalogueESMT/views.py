bd8a682a4ab03087c4be91929f99dbad06675bd4
Saly-Ba <sellyba7@gmail.com>
2021-01-11 02:24:53 +0000

    nouvelle verson 11/01/2020

--- memoire_esmt/catalogueESMT/views.py
+++ memoire_esmt/catalogueESMT/views.py
@@ -1,28 +1,35 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import User,Participant,Animateur,Gestionnaire,Formation,Session,Avis
 from .forms import ParticipantForm,AnimateurForm,GestionnaireForm,FormationForm,SessionForm
+import datetime
 
 
 # General :
 def accueil(request):
+    user = None
     if 'pk' in request.session:
         user = User.objects.get(pk=request.session['pk'])
-    else :
-        user = None
     return render(request,"catalogueESMT/accueil.html",{'user':user})
 
 def login(request):
     if request.method == "POST":
-        user = User.objects.get(username=request.POST['username'])
-        if(user.username == request.POST['username'] and user.password == request.POST['password']):
-            request.session['pk'] = user.pk
-            if user.user_type == 1:
-                return redirect('catalogueESMT:gestionnaire')
-            elif user.user_type == 2:
-                return redirect('catalogueESMT:animateur')
-            else :
-                return redirect('catalogueESMT:participant')
+        try:
+            user = User.objects.get(username=request.POST['username'])
+            if(user.username == request.POST['username'] and user.password == request.POST['password']):
+                request.session['pk'] = user.pk
+                if user.user_type == 1:
+                    return redirect('catalogueESMT:gestionnaire')
+                elif user.user_type == 2:
+                    return redirect('catalogueESMT:animateur')
+                else :
+                    return redirect('catalogueESMT:participant')
+            else:
+                messages.error(request,'Le username ou le mot de passe est incorrect')
+        except:
+            messages.error(request,'Cette utilisateur n\'existe pas dans notre base donnees')
+        
         
     return render(request,'registration/login.html')
 
@@ -66,22 +73,33 @@ def espace_animateur(request):
         animateur = User.objects.get(pk=request.session['pk'])
     return render(request,'catalogueESMT/animateur/espace_animateur.html',{'animateur':animateur})
 
+#Formations Actions:
+def liste_formation(request):
+    periode = datetime.now().year
+    sessions = Session.objects.filter(date__year=periode)
+    formations = Formation.objects.filter(pk=sessions.formations.pk)
+    return render(request,'catalogueESMT/formation/lister_formation.html',{'formations':formations, 'sessions':sessions})
+
 #Gestionnaire Actions :
 def espace_gestionnaire(request):
     gestionnaire = User.objects.get(pk=request.session['pk'])
     return render(request,'catalogueESMT/gestionnaire/espace_gestionnaire.html',{'gestionnaire':gestionnaire})
         
 def ajout_animateur(request):
-    connected_user = User.objects.get(pk=request.session['pk'])
-    if connected_user.user_type == 1:
-        print("Heyyy !!!")
-        if request.method == "POST":
-            form = AnimateurForm(request.POST)
-            if form.is_valid():
-                form.save()
-                return redirect('catalogueESMT:accueil')
+    if not ('pk' in request.session):
+        return redirect('catalogueESMT:login')
+    else:
+        connected_user = User.objects.get(pk=request.session['pk'])
+        if connected_user.user_type == 1:
+            if request.method == "POST":
+                form = AnimateurForm(request.POST)
+                if form.is_valid():
+                    form.save()
+                    return redirect('catalogueESMT:gestionnaire')
+            else:
+                form = AnimateurForm()
         else:
-            form = AnimateurForm()
+            return redirect('catalogueESMT:accueil')
     return render(request,'catalogueESMT/animateur/ajout_animateur.html',{'form':form})
 
 def ajout_formation(request):
@@ -90,15 +108,41 @@ def ajout_formation(request):
     else :
         connected_user = User.objects.get(pk=request.session['pk'])
         if connected_user.user_type == 1:
-            print("Heyyy !!!")
-            if request.method == "POST":
-                form = FormationForm(request.POST)
-                if form.is_valid():
-                    form.save()
-                    return redirect('catalogueESMT:accueil')
+            animateurs = Animateur.objects.all()
+            if animateurs.exists():
+                if request.method == "POST":
+                    form = FormationForm(request.POST)
+                    if form.is_valid():
+                        form.save()
+                        return redirect('catalogueESMT:gestionnaire')
+                else:
+                    form = FormationForm()
             else:
-                form = FormationForm()
-    return render(request,'catalogueESMT/formation/ajout_formation.html',{'form':form})
+                messages.error(request,'Aucun animateurs n\'est encours present dans votre base de donnees')
+        else:
+            return redirect('catalogueESMT:accueil')
+    return render(request,'catalogueESMT/formation/ajout_formation.html',{'form':form, 'animateurs':animateurs})
+
+def ajout_session(request):
+    if not ('pk' in request.session):
+        return redirect('catalogueESMT:login')
+    else :
+        connected_user = User.objects.get(pk=request.session['pk'])
+        if connected_user.user_type == 1:
+            formations = Formation.objects.all()
+            if formations :
+                if request.method == "POST":
+                    form = SessionForm(request.POST)
+                    if form.is_valid():
+                        form.save()
+                        return redirect('catalogueESMT:gestionnaire')
+                else :
+                    form = SessionForm()
+            else:
+                messages.error(request,'Il y\' a, pour le moment, aucune formation enregistrer dans votre base de données')
+        else:
+            return redirect('catalogueESMT:accueil')
+    return render(request,'catalogueESMT/formation/ajout_session.html',{'formations':formations})
 
 def action_animateur(request):
     connected_user = User.objects.get(pk=request.session['pk'])
